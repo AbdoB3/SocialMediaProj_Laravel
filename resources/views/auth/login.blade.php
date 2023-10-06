5dc9b5ae3d172465b070a806343326089331b0a7
ladox <mouadin.khalid@gmail.com>
2023-10-07 00:47:06 +0200

    ladox update test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
             @csrf
 
             <div class="mb-4">
-                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Email Address') }}</label>
+                <label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Address') }}</label>
                 <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') border-red-500 @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                 @error('email')
                 <p class="text-red-500 text-xs italic">{{ $message }}</p>
